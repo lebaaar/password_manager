0eda05b76bf523e8fcd54747afdecdd416d451ef
Lan <lanlebar5@gmail.com>
2024-07-06 13:53:08 +0200

    Manage categories functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 import sys
+import json
 
 def install_and_import(package):
     try:
@@ -38,6 +39,7 @@ class PasswordManagerApp:
         self.root.title("Password Manager")
         self.key = None
         self.show_initial_screen()
+        self.ensure_files()
 
     def check_key_presence(self):
         try:
@@ -45,6 +47,53 @@ class PasswordManagerApp:
             salt_exists = os.path.exists("salt.bin") and os.path.getsize("salt.bin") > 0
             return key_exists and salt_exists
         except:
+            return False
+    
+    def ensure_files(self):
+        if not os.path.exists("vault"):
+            os.mkdir("vault")
+        if not os.path.exists("categories.json"):
+            with open("categories.json", "w") as file:
+                file.write("[]")
+        if not os.path.exists("vault.json"):
+            with open("vault.json", "w") as file:
+                file.write("{}")
+        
+    def get_categories(self):
+        with open("categories.json", "r") as file:
+            return json.load(file)
+
+    def add_category(self, category):
+        categories = self.get_categories()
+        if category not in categories:
+            categories.append(category)
+            with open("categories.json", "w") as file:
+                json.dump(categories, file)
+        else:
+            messagebox.showerror("Error", "Category already exists")
+
+    def remove_category(self, category):
+        categories = self.get_categories()
+        if category in categories:
+            categories.remove(category)
+            with open("categories.json", "w") as file:
+                json.dump(categories, file)
+        else:
+            messagebox.showerror("Error", "Category does not exist")
+
+    def rename_category(self, old_category, new_category):
+        categories = self.get_categories()
+        if new_category in categories:
+            messagebox.showerror("Error", "Category already exists")
+            return False
+        if old_category in categories:
+            categories.remove(old_category)
+            categories.append(new_category)
+            with open("categories.json", "w") as file:
+                json.dump(categories, file)
+            return True
+        else:
+            messagebox.showerror("Error", "Category does not exist")
             return False
 
     def clear_screen(self):
@@ -87,22 +136,22 @@ class PasswordManagerApp:
         else:
             messagebox.showerror("Error", "Passwords do not match")
 
-    def adjust_window_size(self):
-        self.root.update_idletasks()
-        window_width = self.root.winfo_reqwidth()
-        window_height = self.root.winfo_reqheight()
-        screen_width = self.root.winfo_screenwidth()
-        screen_height = self.root.winfo_screenheight()
-        window_width += 50
-        window_height += 50
+    def adjust_window_size(self, window=None):
+        target_window = self.root if window is None else window
+        target_window.update_idletasks()
+        window_width = target_window.winfo_reqwidth()
+        window_height = target_window.winfo_reqheight()
+        screen_width = target_window.winfo_screenwidth()
+        screen_height = target_window.winfo_screenheight()
+        window_width += 50  # Adjust dimensions as needed
+        window_height += 50  # Adjust dimensions as needed
         x = (screen_width / 2) - (window_width / 2)
         y = (screen_height / 2) - (window_height / 2)
-        self.root.geometry(f"{int(window_width)}x{int(window_height)}+{int(x)}+{int(y)}")
+        target_window.geometry(f"{int(window_width)}x{int(window_height)}+{int(x)}+{int(y)}")
 
     def show_initial_screen(self):
         self.clear_screen()
         self.root.bind("<Escape>", lambda _: self.root.destroy())
-
 
         key_present = self.check_key_presence()
         if key_present:
@@ -144,7 +193,6 @@ class PasswordManagerApp:
         sort_frame.pack(pady=5)
 
         ttk.Label(sort_frame, text="Sort:").pack(side='left')
-        
         sort_dropdown = ttk.Combobox(sort_frame, values=["Date", "Name"], state="readonly")
         sort_dropdown.current(0)
         sort_dropdown.pack(side='left')
@@ -194,6 +242,8 @@ class PasswordManagerApp:
             frame = ttk.Frame(self.password_display)
             frame.pack(fill='x', pady=2)
             ttk.Label(frame, text=f"{service}: {password}", anchor='w').pack(side='left', fill='x', expand=True)
+
+            # Buttons
             copy_button = ttk.Button(frame, text="Copy", command=lambda p=password: copy_password(p))
             copy_button.pack(side='left')
             copy_button.bind("<Return>", lambda _: copy_button.invoke())
@@ -206,28 +256,65 @@ class PasswordManagerApp:
             delete_button.pack(side='left')
             delete_button.bind("<Return>", lambda _: delete_button.invoke())
 
-    def show_password_setter(self, service=None, password=None):
+
+    def show_password_setter(self, service=None, username=None, category=None, password=None):
         change_mode = service is not None and password is not None
         original_service = service
         
-        password_window = tkinter.Toplevel(self.root)
+        self.password_window = tkinter.Toplevel(self.root)
         if change_mode:
-            password_window.title("Change Password")
+            self.password_window.title("Change Password")
         else:
-            password_window.title("Add Password")
-        password_window.bind("<Escape>", lambda _: password_window.destroy())
-        password_window.geometry("400x200")
+            self.password_window.title("Add Password")
+        try:
+            if self.window_position:
+                x, y = self.window_position
+                self.password_window.geometry(f"400x400+{x}+{y}")
+            self.password_window.title("Manage Categories")
+        except Exception:
+            self.password_window.geometry("400x400")
+            pass
+        self.password_window.bind("<Escape>", lambda _: self.password_window.destroy())
 
-        ttk.Label(password_window, text="Enter Service:").pack(pady=10)
-        service_entry = ttk.Entry(password_window, width=30)
+        # Service
+        ttk.Label(self.password_window, text="Enter Service*:").pack(pady=10)
+        service_entry = ttk.Entry(self.password_window, width=30)
         service_entry.pack(pady=5)
         service_entry.focus_set()
         service_entry.bind("<Return>", lambda _: submit_password())
         if service:
             service_entry.insert(0, service)
 
-        ttk.Label(password_window, text="Enter Password:").pack(pady=10)
-        password_entry = ttk.Entry(password_window, width=50)
+        # Username
+        ttk.Label(self.password_window, text="Enter username:").pack(pady=10)
+        username_entry = ttk.Entry(self.password_window, width=50)
+        username_entry.pack(pady=5)
+        if username:
+            username_entry.insert(0, username)
+
+        # Category
+        category_frame = ttk.Frame(self.password_window)
+        category_frame.pack(pady=5)
+
+        ttk.Label(category_frame, text="Category:").pack(side='left')
+        cateories = self.get_categories()
+        cateories.insert(0, "None")
+        sort_dropdown = ttk.Combobox(category_frame, values=cateories, state="readonly")
+        sort_dropdown.current(0)
+        sort_dropdown.pack(side='left')
+        if category:
+            sort_dropdown.set(category)
+        
+        # Add category button
+        ttk.Button(category_frame, text="Manage Categories", command=lambda: self.show_manage_categories()).pack(side='left')
+
+        # Notes
+        ttk.Label(self.password_window, text="Enter notes:").pack(pady=10)
+        notes_entry = ttk.Entry(self.password_window, width=50)
+        notes_entry.pack(pady=5)
+
+        ttk.Label(self.password_window, text="Enter Password*:").pack(pady=10)
+        password_entry = ttk.Entry(self.password_window, width=50)
         password_entry.pack(pady=5)
         password_entry.bind("<Return>", lambda _: submit_password())
         if password:
@@ -243,21 +330,127 @@ class PasswordManagerApp:
                 self.save_password(original_service, password, True, None, service)
             else:
                 self.save_password(service, password)
-            password_window.destroy()
+            self.password_window.destroy()
             self.update_password_display()
 
-        save_button = ttk.Button(password_window, text="Save", command=submit_password)
+        save_button = ttk.Button(self.password_window, text="Save", command=submit_password)
         save_button.pack(pady=10)
         save_button.bind("<Return>", lambda _: submit_password())
 
-        password_window.bind("<Escape>", lambda _: password_window.destroy())
+        self.password_window.bind("<Escape>", lambda _: self.password_window.destroy())
 
-    def delete_password(self, service):
-        sure = messagebox.askyesno("Are you sure?", f"Are you sure you want to delete the password for {service}?")
-        if sure:
-            os.remove(f"vault/{service}.enc")
-            self.update_password_display()
-            self.adjust_window_size()
+    def on_manage_categories_window_close(self, setter_x, setter_y):
+        self.window_position = (setter_x, setter_y)
+        self.manage_categories_window.destroy()
+        try: 
+            self.password_window.destroy()
+        except:
+            pass
+        self.show_password_setter()
+
+    def show_manage_categories(self):
+        self.manage_categories_window = tkinter.Toplevel(self.root)
+        self.manage_categories_window.protocol("WM_DELETE_WINDOW", lambda: self.on_manage_categories_window_close(self.password_window.winfo_x(), self.password_window.winfo_y()))
+        try:
+            if self.window_position:
+                x, y = self.window_position
+                self.manage_categories_window.geometry(f"400x200+{x}+{y}")
+            self.manage_categories_window.title("Manage Categories")
+        except Exception:
+            self.manage_categories_window.geometry("400x200")
+            pass
+        self.manage_categories_window.bind("<Escape>", lambda _: self.on_manage_categories_window_close())
+        
+        def delete_category(category):
+            self.remove_category(category)
+            x = self.manage_categories_window.winfo_x()
+            y = self.manage_categories_window.winfo_y()
+            self.window_position = (x, y)
+            self.manage_categories_window.destroy()
+            self.show_manage_categories()
+
+        categories = self.get_categories()
+        for category in categories:
+            frame = ttk.Frame(self.manage_categories_window)
+            frame.pack(fill='x', pady=2)
+            ttk.Label(frame, text=category, anchor='w').pack(side='left', fill='x', expand=True)
+
+            # Buttons
+            change_button = ttk.Button(frame, text="Rename", command=lambda c=category: self.show_rename_category(category=c))
+            change_button.pack(side='left')
+            change_button.bind("<Return>", lambda _: change_button.invoke())
+
+            delete_button = ttk.Button(frame, text="Delete", command=lambda c=category: delete_category(c))
+            delete_button.pack(side='left')
+            delete_button.bind("<Return>", lambda _: delete_button.invoke())
+
+        def add_category():
+            category = category_entry.get()
+            if not category:
+                print("Category cannot be empty")
+                messagebox.showerror("Error", "Category cannot be empty")
+                try:
+                    category_entry.focus_set()
+                except:
+                    self.manage_categories_window.focus_set()
+            else:
+                self.add_category(category)
+                x = self.manage_categories_window.winfo_x()
+                y = self.manage_categories_window.winfo_y()
+                self.window_position = (x, y)
+                self.manage_categories_window.destroy()
+                self.show_manage_categories()
+
+        # Add category entry
+        ttk.Label(self.manage_categories_window, text="Add new category:").pack(pady=10)
+        category_entry = ttk.Entry(self.manage_categories_window, width=50)
+        category_entry.pack(pady=5)
+        category_entry.focus_set()
+        category_entry.bind("<Return>", lambda _: add_category())
+
+        # Add category button
+        add_category_button = ttk.Button(self.manage_categories_window, text="Add Category", command=add_category)
+        add_category_button.pack(pady=10)
+        add_category_button.bind("<Return>", lambda _: add_category())
+
+        self.adjust_window_size(self.manage_categories_window)
+
+    def show_rename_category(self, category):
+        rename_window = tkinter.Toplevel(self.root)
+        rename_window.title("Rename Category")
+        rename_window.bind("<Escape>", lambda _: rename_window.destroy())
+        rename_window.geometry("400x300")
+
+        def rename_category():
+            x = self.manage_categories_window.winfo_x()
+            y = self.manage_categories_window.winfo_y()
+            self.window_position = (x, y)
+            new_category = category_entry.get()
+            if not new_category:
+                messagebox.showerror("Error", "Category cannot be empty")
+                return
+            if new_category == category:
+                rename_window.destroy()
+                return
+            
+            if self.rename_category(category, new_category):
+                # get current window position
+
+                rename_window.destroy()
+                self.manage_categories_window.destroy()
+                self.show_manage_categories()
+
+        ttk.Label(rename_window, text="Category name:").pack(pady=10)
+        category_entry = ttk.Entry(rename_window, width=50)
+        category_entry.pack(pady=5)
+        category_entry.insert(0, category)
+        category_entry.focus_set()
+        category_entry.bind("<Return>", lambda _: rename_category())
+
+        rename_category_button = ttk.Button(rename_window, text="Rename Category", command=rename_category)
+        rename_category_button.pack(pady=10)
+        rename_category_button.bind("<Return>", lambda _: rename_category())
+
 
     def show_change_master_password(self):
         change_window = tkinter.Toplevel(self.root)
@@ -338,6 +531,13 @@ class PasswordManagerApp:
         
         self.update_password_display()
         self.adjust_window_size()
+
+    def delete_password(self, service):
+        sure = messagebox.askyesno("Are you sure?", f"Are you sure you want to delete the password for {service}?")
+        if sure:
+            os.remove(f"vault/{service}.enc")
+            self.update_password_display()
+            self.adjust_window_size()
 
 def main():
     root = ThemedTk(theme="arc")
